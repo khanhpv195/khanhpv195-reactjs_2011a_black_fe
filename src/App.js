1da1eff2afa9dd4a1d4f8451a7a66b2b12762fab
The Doctor <karuyanakaido24@gmail.com>
2020-12-23 20:57:52 +0700

    Update b12

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Button from './components/atoms/Button/Button';
 function App() {
+  function onClickHandle() {
+    alert("OK")
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Button type="button"
+      onClick={onClickHandle}
+      className="A">
+      Send
+    </Button>
   );
 }
 
